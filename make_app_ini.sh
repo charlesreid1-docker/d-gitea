8149a708150226c4e8eec01e0731330427788209
Charles Reid <charlesreid1@gmail.com>
2020-04-27 11:36:38 -0700

    fix file location in jinja render script

--- make_app_ini.sh
+++ make_app_ini.sh
@@ -4,6 +4,6 @@ set -x
 # secret_key and internal_token should be SIMPLE or this 
 # gets screwy, and I don't want to do any more debugging.
 # 
-cat app.ini.sample \
+cat custom/conf/app.ini.sample \
   | sed 's/REPLACEME_INTERNALTOKEN_SECRET/'$(cat internal_token.secret)'/g' \
   | sed 's/REPLACEME_SECRETKEY_SECRET/'$(cat secret_key.secret)'/g'  > custom/conf/app.ini
