95cbc571d791a31cfdde3ab5a8c047104b2b0fe0
Charles Reid <charlesreid1@gmail.com>
2021-09-08 15:37:49 -0700

    try again?

--- make_app_ini.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-# secret_key and internal_token should be SIMPLE or this 
-# gets screwy, and I don't want to do any more debugging.
-# 
-cat custom/conf/app.ini.sample \
-  | sed 's/REPLACEME_INTERNALTOKEN_SECRET/'$(cat internal_token.secret)'/g' \
-  | sed 's/REPLACEME_SECRETKEY_SECRET/'$(cat secret_key.secret)'/g'  > custom/conf/app.ini
